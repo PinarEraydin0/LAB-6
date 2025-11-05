a0eb8bdbec186582a2901ee94d3db3813e332a60
PinarEraydin0 <240201012@sivas.edu.tr>
2025-11-05 19:30:47 +0300

    Add files via upload

---
+++ t3.cpp
@@ -0,0 +1,33 @@
+﻿
+#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+double getAverage(string filename) {
+    ifstream file(filename);
+    int numbers[1000]; 
+    int count = 0;
+
+    int num;
+    while (file >> num) {
+        numbers[count] = num;
+        count++;
+    }
+
+    if (count == 0) return 0; 
+   double sum = 0;
+    for (int i = 0; i < count; i++) {
+        sum += numbers[i];
+    }
+
+    return (sum) / count; 
+}
+
+int main() {
+    string filename = "grades.txt";
+    double average = getAverage(filename);
+    cout << "Ortalama: " << average << endl;
+    return 0;
+}
+
