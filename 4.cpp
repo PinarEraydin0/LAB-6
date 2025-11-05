4682f2ce4bd99844d60582e2b0a334f15a63f398
PinarEraydin0 <240201012@sivas.edu.tr>
2025-11-05 19:32:27 +0300

    Add files via upload

---
+++ 4.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+void printInfo(string name, int age) {
+    cout << "Name: " << name << ", Age: " << age << endl;
+}
+
+void printInfo(int code, string course) {
+    cout << "Course: " << course << ", Code: " << code << endl;
+}
+
+int main() {
+    printInfo("Ali", 20);         
+    printInfo(101, "Programming"); 
+    return 0;
+}
+
+
+
